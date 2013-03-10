d2530664d621321cdc7974317684e4a80275dd90
Thomas Feldmann <feldmann.thomas@googlemail.com>
2013-03-10 19:46:17 +0100

    documentation

--- thomdo.py
+++ thomdo.py
@@ -23,16 +23,19 @@ class Thomdo(QtGui.QDialog):
         self.ui.loadButton.clicked.connect(self.load)
 
     def clear(self):
+        """ clears checked items """
         for item in self._tasks():
             if item.checkState() == QtCore.Qt.Checked:
                 row = self.ui.taskList.row(item)
                 self.ui.taskList.takeItem(row)
 
     def save(self):
+        """ saves the task as a json file """
         with open("tasks.json", "w") as f:
             json.dump(self._tasks_dict(), f)
 
     def load(self):
+        """ parses a saved json task file and fills the list """
         with open("tasks.json", 'rb') as f:
             tasks = json.loads(f.read())
             self.ui.taskList.clear()
@@ -40,24 +43,28 @@ class Thomdo(QtGui.QDialog):
                 self._add_task(task['title'], task['done'])
 
     def return_pressed(self):
+        """ clears the line edit and inserts the task """
         entered_text = self.ui.entryField.text()
         if entered_text != "":
             self.ui.entryField.clear()
             self._add_task(entered_text, False)
 
     def _tasks(self):
+        """ returns all tasks as a list of QListWidgetItems """
         tasklist = self.ui.taskList
         return [tasklist.item(i) for i in range(tasklist.count())]
 
     def _tasks_dict(self):
-        json_tasks = []
+        """ returns all tasks as array of dictionaries """
+        dicts = []
         for task in self._tasks():
             done = (task.checkState() == QtCore.Qt.Checked)
-            json_task = {"title": task.text(), "done": done}
-            json_tasks.insert(0, json_task)
-        return json_tasks
+            dict = {"title": task.text(), "done": done}
+            dicts.insert(0, dict)
+        return dicts
 
     def _add_task(self, title, checked):
+        """ inserts a task at the beginning of the ui.taskList widget """
         item = QtGui.QListWidgetItem()
         item.setText(title)
         if checked:
